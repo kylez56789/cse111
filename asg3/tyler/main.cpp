1a8065b129349cb258165f94c4b45015df85c64b
unknown <tystran@ucsc.edu>
2020-02-14 01:07:59 -0800

    hi

--- asg3/tyler/main.cpp
+++ asg3/tyler/main.cpp
@@ -46,63 +46,71 @@ void process_file(istream& infile, const string& filename,
    int linenum = 1;
    for(;;) {
       string line;
-      cout << filename << ": " << linenum << ": ";
-      getline(infile, line);
       if (infile.eof()) {
          break;
       }
-      //cout << filename << ": " << linenum << ": ";
+      cout << filename << ": " << linenum << ": ";
+      getline(infile, line);
+      cout << line << endl;
+      //if (infile.eof()) {
+      //   break;
+      //}
       smatch result;
       if (regex_search (line, result, comment_regex)) {
-         //cout << "Comment or empty line." << endl;
+         linenum++;
+         //cout << endl;
          continue;
       }
       if (regex_search (line, result, key_value_regex)) {
-         //cout << "key  : \"" << result[1] << "\"" << endl;
-         //cout << "value: \"" << result[2] << "\"" << endl;
          if(result[1] == "" && result[2] == "") {
+            bool printed = false;
             for (str_str_map::iterator itor = list.begin();
-                 itor != list.end(); ++itor) {
-                cout<<itor->first<<" = "<<itor->second<<endl;
+               itor != list.end(); ++itor) {
+               cout<<itor->first<<" = "<<itor->second<<endl;
+               printed = true;
+            }
+            if(printed == false) {
+               //cout << endl;
             }
          }
          else if (result[1] != "" && result[2] == "") {
             str_str_map::iterator itor = list.find(result[1]);
             if (itor != list.end()) {
                list.erase(itor);
-            }
-            else {
-               cout << "Error msg for deleting node not in map?\n";
-            }   
+            } 
+            //cout << endl;  
          }
          else if (result[1] == "" && result[2] != "") {
+            bool printed = false;
             for (str_str_map::iterator itor = list.begin();
                  itor != list.end(); ++itor) {
                if((*itor).second == result[2]) {
                   cout<<itor->first<<" = "<<itor->second<<endl;
+                  printed = true;
                }
+            }
+            if(printed == false) {
+               //cout << endl;
             }
          }
          else {
-            //cout << "Insert based on key or replace value" << endl;
             str_str_pair to_be_inserted (result[1],result[2]);
             list.insert(to_be_inserted);
-            cout << result[1] << " = " << result[2];
+            cout << result[1] << " = " << result[2]<<endl;
          }
       }else if (regex_search (line, result, trimmed_regex)) {
          str_str_map::iterator itor = list.find(result[1]);
          if (itor == list.end()) {
-            cerr << result[1] << ": key not found";
+            cerr << result[1] << ": key not found"<<endl;
          }
          else {
-             cout<<itor->first<<" = "<<itor->second;
+             cout<<itor->first<<" = "<<itor->second<<endl;
          }   
          
       }else {
          assert (false and "This can not happen.");
       }
       linenum++;
-      cout << endl;
    }   
 }
 
@@ -111,34 +119,26 @@ int main (int argc, char** argv) {
    scan_options (argc, argv);
    str_str_map list {};
    vector<string> filenames;  
-   //str_str_map test {};
    for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
       str_str_pair pair (*argp, to_string<int> (argp - argv));
       filenames.push_back(*argp);
-      //cout << "Before insert: " << pair << endl;
-      //test.insert (pair);
    }
 
    if(filenames.size() == 0) {
       filenames.push_back(cin_name);
    }
-   /*
-   for(int a = 0; a < filenames.size(); a++) {
-      cout << "filenames " << a << ":" << filenames[a] << endl;
-   }
-   */
    for(const auto& filename: filenames) {
       if(filename == cin_name) {
-         //cout << "Reading from cin" << endl;
          process_file(cin, filename, list);
       }
    else {
          ifstream infile (filename);
          if(infile.fail()) {
-            cerr << "No such file or directory" << endl;
+            complain() << filename << 
+               ": No such file or directory" << endl;
+            sys_info::exit_status(1);
          }
          else {
-            //cout << "Read from file " << filename << endl;
             process_file(infile, filename, list);
             infile.close();
          }
@@ -146,8 +146,6 @@ int main (int argc, char** argv) {
    }
    str_str_map::iterator itor = list.begin();
    list.erase (itor);
-
-   cout << "EXIT_SUCCESS" << endl;
-   return EXIT_SUCCESS;
+   return sys_info::exit_status();
 }
 
