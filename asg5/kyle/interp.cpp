d7a3fecff03e6d05326589c88f321d5b55274132
unknown <kylez56789@gmail.com>
2020-03-12 12:18:05 -0700

    asg5

---
+++ asg5/kyle/interp.cpp
@@ -0,0 +1,153 @@
+// $Id: interp.cpp,v 1.3 2019-03-19 16:18:22-07 - - $
+
+#include <memory>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <GL/freeglut.h>
+
+#include "debug.h"
+#include "interp.h"
+#include "shape.h"
+#include "util.h"
+
+unordered_map<string,interpreter::interpreterfn>
+interpreter::interp_map {
+   {"define" , &interpreter::do_define },
+   {"draw"   , &interpreter::do_draw   },
+};
+
+unordered_map<string,interpreter::factoryfn>
+interpreter::factory_map {
+   {"text"     , &interpreter::make_text     },
+   {"ellipse"  , &interpreter::make_ellipse  },
+   {"circle"   , &interpreter::make_circle   },
+   {"polygon"  , &interpreter::make_polygon  },
+   {"rectangle", &interpreter::make_rectangle},
+   {"square"   , &interpreter::make_square   },
+	{"diamond"	, &interpreter::make_diamond	},
+	{"triangle" , &interpreter::make_triangle },
+	{"equilateral", &interpreter::make_equilateral },
+};
+
+interpreter::shape_map interpreter::objmap;
+
+interpreter::~interpreter() {
+   for (const auto& itor: objmap) {
+      cout << "objmap[" << itor.first << "] = "
+           << *itor.second << endl;
+   }
+}
+
+void interpreter::interpret (const parameters& params) {
+   DEBUGF ('i', params);
+   param begin = params.cbegin();
+   string command = *begin;
+   auto itor = interp_map.find (command);
+   if (itor == interp_map.end()) throw runtime_error ("syntax error");
+   interpreterfn func = itor->second;
+   func (++begin, params.cend());
+}
+
+void interpreter::do_define (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+   string name = *begin;
+   objmap.emplace (name, make_shape (++begin, end));
+}
+
+
+void interpreter::do_draw (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+   if (end - begin != 4) throw runtime_error ("syntax error");
+   string name = begin[1];
+   shape_map::const_iterator itor = objmap.find (name);
+   if (itor == objmap.end()) {
+      throw runtime_error (name + ": no such shape");
+   }
+   rgbcolor color {begin[0]};
+   vertex where {from_string<GLfloat> (begin[2]),
+                 from_string<GLfloat> (begin[3])};
+   window::push_back (object (itor->second, where, color));
+}
+
+shape_ptr interpreter::make_shape (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+   string type = *begin++;
+   auto itor = factory_map.find(type);
+   if (itor == factory_map.end()) {
+      throw runtime_error (type + ": no such shape");
+   }
+   factoryfn func = itor->second;
+   return func (begin, end);
+}
+
+shape_ptr interpreter::make_text (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+	string text = "";
+	string font = *begin++;
+	while (begin != end) {
+		text = text + (*begin++);
+		text += " ";
+	}
+   return make_shared<text> (font, text);
+}
+
+shape_ptr interpreter::make_ellipse (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+	GLfloat width = strtof((*begin++).c_str(), 0);
+	GLfloat height = strtof((*begin).c_str(), 0);
+   return make_shared<ellipse> (width, height);
+}
+
+shape_ptr interpreter::make_circle (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+	Glfloat diameter = strtof((*begin).c_str(), 0);
+   return make_shared<circle> (diameter);
+}
+
+shape_ptr interpreter::make_polygon (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+	vertex_list vertexes;
+	Glfloat xcoord;
+	Glfloat ycoord;
+	while (begin != end) {
+		xcoord = strtof((*begin++).c_str(), 0);
+		ycoord = strtof((*begin++).c_str(), 0);
+		vertexes.push_back({xcoord, ycoord});
+	}
+   return make_shared<polygon> (vertexes);
+}
+
+shape_ptr interpreter::make_rectangle (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+	Glfloat width = strtof((*begin++).c_str(), 0);
+	Glfloat height = strtof((*begin).c_str(), 0);
+   return make_shared<rectangle> (width, height);
+}
+
+shape_ptr interpreter::make_square (param begin, param end) {
+   DEBUGF ('f', range (begin, end));
+	Glfloat side = strtof((*begin).c_str(), 0);
+   return make_shared<square> (side);
+}
+
+shape_ptr interpreter::make_diamond(param begin, param end) {
+	DEBUGF ('f', range (begin, end));
+   GLfloat width = strtof((*begin++).c_str(), 0);
+   GLfloat height = strtof((*begin).c_str(), 0);
+   return make_shared<diamond> (width, height);
+
+}
+
+shape_ptr interpreter::make_triangle(param begin, param end) {
+	DEBUGF ('f', range (begin, end));
+   GLfloat width = strtof((*begin).c_str(), 0);
+   return make_shared<triangle>(width);
+}
+
+shape_ptr interpreter::make_equilateral(param begin, param end) {
+	DEBUGF ('f', range (begin, end));
+	GLfloat width = strtof((*begin).c_str(), 0);	
+	return make_shared<equilateral>(width);
+}
